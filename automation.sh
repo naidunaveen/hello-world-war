49b98f3a15f60ac04fc527985989e67e164a1901
naidunaveen <146354959+naidunaveen@users.noreply.github.com>
2023-10-19 21:18:52 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 sudo apt update -y
 sudo apt install default-jdk -y
 sudo apt-cache search tomcat
-sudo apt install tomcat9 tomcat9-admin
+sudo apt install tomcat9 tomcat9-admin -y
 sudo ufw allow from any to any port 9090 proto tcp
-sudo sed -i 's/connector port="8080"/connector port="8090"/Ig' /etc/tomcat9/server.xml
+sudo sed -i 's/connector port="8080"/connector port="9090"/Ig' /etc/tomcat9/server.xml
 	// Change the port number from 8080 to 9090
 sudo sed -i '56 i <role rolename="admin-gui"/>'  /etc/tomcat9/tomcat-users.xml
 	// inside users.xml paste below commands and allign properly
 sudo sed -i '57 i <role rolename="manager-gui"/>' /etc/tomcat9/tomcat-users.xml
-sudo sed -i '57 i <role rolename="tomcat" password="pass" roles="admin-gui,manager-gui"/>' /etc/tomcat9/tomcat-users.xml
+sudo sed -i '58 i <role rolename="tomcat" password="pass" roles="admin-gui,manager-gui"/>' /etc/tomcat9/tomcat-users.xml
 
 
